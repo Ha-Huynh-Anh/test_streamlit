a962bc2da8230545d80d8d1cf70cfd575747c1bc
Ha-Huynh-Anh <huynhanh.ha94@gmail.com>
2021-07-08 22:11:03 +0700

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -21,9 +21,21 @@ def trans_sym(list_sym):
             list_eng.append(list(sym_df.eng)[index])
     return list_eng
 
+# convert to array format
+def sym_to_vector(list_input_sym):
+    #  to convert sym to vector
+    sym_input_vector = []    
+    for i in list(sym_df.eng): 
+        if i in list_input_sym:
+            sym_input_vector.append(1)
+        else:
+            sym_input_vector.append(0)
+    return sym_input_vector
+
 url_sym_df = 'https://raw.githubusercontent.com/Ha-Huynh-Anh/Timbenhvien_test/main/data/sym_df.csv'
 sym_df = get_sym_df(url_sym_df)
 
 sym_input_vie = st.multiselect('Các triệu chứng của bạn', sym_df.viet.values)
 st.write(sym_input_vie)
 st.write(trans_sym(sym_input_vie))
+st.write(sym_to_vector(sym_input_vie))
