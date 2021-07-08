89d6e37966d28beaaacdba28728a60c6367c10a9
Ha-Huynh-Anh <huynhanh.ha94@gmail.com>
2021-07-08 22:08:31 +0700

    Update test_streamlit.py

--- test_streamlit.py
+++ test_streamlit.py
@@ -8,8 +8,22 @@ def get_sym_df(url1):
   sym_df = sym_df.loc[:,('eng','viet')]
   return sym_df
 
+def trans_sym(list_sym):
+    """
+    translate sym from list vie to list eng
+    input: ['Yếu mệt','Da nổi mẩn']
+    output: ['lethargy', 'nodal_skin_eruptions']
+    """
+    list_eng = []
+    for i in list_sym:
+        if i in list(sym_df.viet):
+            index = list(sym_df.viet).index(i)
+            list_eng.append(list(sym_df.eng)[index])
+    return list_eng
+
 url_sym_df = 'https://raw.githubusercontent.com/Ha-Huynh-Anh/Timbenhvien_test/main/data/sym_df.csv'
 sym_df = get_sym_df(url_sym_df)
 
 sym_input_vie = st.multiselect('Các triệu chứng của bạn', sym_df.viet.values)
 st.write(sym_input_vie)
+st.write(trans_sym(sym_input_vie))
